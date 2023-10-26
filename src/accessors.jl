8fadaef0ee1e3b0d7d10f3a0644ac7581615681d
Timothy Knab <knabt@metrumrg.com>
2023-10-26 16:43:45 -0400

    Fixed some things

--- src/accessors.jl
+++ src/accessors.jl
@@ -5,7 +5,7 @@
     else
         if !isnothing(x._valmap)
             mergeddict = merge(x._valmap, x._uvalues)
-            # out = Symbolics.value(substitute(ModelingToolkit.getdefault(x.value), mergeddict))
+
             out = Symbolics.value(substitute(x._valmap[x.value], mergeddict))
             if x.value in keys(x._uvalues)
                 out = x._uvalues[x.value]
@@ -48,18 +48,26 @@ function names(mvals::ModelValues; symbolic=true)
 end
 
 function values(mvals::ModelValues; symbolic=false)
-    if symbolic
-        mergeddict = merge(mvals._valmap, mvals._uvalues)
-        # out = [Symbolics.value(substitute(mvals._values[x].value, mergeddict)) for x in keys(mvals._values)]
-        out = [mvals._values[x].value for x in mvals.names]
-        return out
-    else
-        mergeddict = merge(mvals._valmap, mvals._uvalues)
-        # out = [Symbolics.value(substitute(mvals._values[x].value, mergeddict)) for x in mvals.names]
-        out = [Symbolics.value(Symbolics.substitute(mvals._valmap[x], mergeddict)) for x in keys(mvals._valmap) if Symbol(x) in mvals.names]
-        return out
-    end
+    [getproperty(mvals, nm)+0.0 for nm in mvals.names]
 end
+#     if symbolic
+#         mergeddict = merge(mvals._valmap, mvals._uvalues)
+#         # out = [Symbolics.value(substitute(mvals._values[x].value, mergeddict)) for x in keys(mvals._values)]
+#         out = [mvals._values[x].value for x in mvals.names]
+#         return out
+#     else
+
+#         mergeddict = merge(mvals._valmap, mvals._uvalues)
+#         out = Real[]
+#         for nm in keys(mvals._valmap)
+#             if nm in keys(mergeddict)
+#                 push!(out, mergeddict[out])
+
+
+#         # out = [getproperty(mvals, x).value => PMParameterizedBase.getNumericValue(getproperty(mvals,x)) for x in mvals.names]
+#         return out
+#     end
+# end
 
 
 ## Default for printing parametes is to get the value
